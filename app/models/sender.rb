d99185a09dd3bb6eab55394e47a27fc72274fee7
Brett Hoyer <jbh5079@gmail.com>
2012-11-17 11:07:56 -0600

    Initial Commit

---
+++ app/models/sender.rb
@@ -0,0 +1,3 @@
+class Sender < ActiveRecord::Base
+  attr_accessible :email, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swc::Application.config.secret_token = 'c3698cc6c3a7593a35f2496eae5201de3d46e043ca4d29a7d84f0ab34f88d82d8197df306962c7d4a1f5dc63349bd5fce9d6497a03db6f998c16c98d7e4d0e4e'
